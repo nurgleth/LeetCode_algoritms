ce36fc9ac2174bcd38a2806df1f8b863d81c0797
Nurgle <axilles90@gmail.com>
2021-04-15 21:49:15 +0300

    Функция принимает строку и меняет найденный элемент в ней методом replace

---
+++ 1108 Defanging an IP Address.py
@@ -0,0 +1,25 @@
+"""
+Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+A defanged IP address replaces every period "." with "[.]".
+
+Example 1:
+
+Input: address = "1.1.1.1"
+Output: "1[.]1[.]1[.]1"
+"""
+
+
+def defangIPaddr(address):
+    """
+    :type address: str
+    :rtype: str
+    """
+    b = address.replace(".", "[.]")
+
+    return b
+
+
+a = "1.1.1.1"
+
+
